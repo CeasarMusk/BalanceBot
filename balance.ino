be246ea436c5f916f8a8a6b8aa27d917dd979aa7
CeasarMusk <ceasarmusk@gmail.com>
2026-08-24 17:56:29 -0400

    Changed Motor Driver, test WORKS FUCK YEA

--- balance.ino
+++ balance.ino
@@ -25,40 +25,42 @@
 // ╔══════════════════════════════════════════════════════════╗
 // ║                  Init Pins                               ║
 // ╚══════════════════════════════════════════════════════════╝
-#define LED_BUILTIN 2
-#define PWMA 15
-#define AIN2 2
-#define AIN1 4
-#define BIN1 16
-#define BIN2 5
-#define PWMB 17
 #define SCL 22
 #define SDA 21
 
 // ╔══════════════════════════════════════════════════════════╗
 // ║                  Motor Shit                              ║
 // ╚══════════════════════════════════════════════════════════╝
-#define FREQ 30000
+#define FREQ 20000
 #define PWMCHANNEL 0
 #define RESOLUTION 8 
 #define DUTYCYCLE 220
 
+#define DIR1 33
+#define DIR2 26
+#define PMW1 25
+#define PMW2 32
 
 
-Adafruit_MPU6050 mpu;
+//Adafruit_MPU6050 mpu;
 AsyncWebServer server(80);
 
 void setup(void) {
 // ╔══════════════════════════════════════════════════════════╗
 // ║                  Init Pins                               ║
 // ╚══════════════════════════════════════════════════════════╝
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(PWMB,OUTPUT);
-  pinMode(PWMA,OUTPUT);
-  pinMode(AIN2,OUTPUT);
-  pinMode(AIN1,OUTPUT);
-  pinMode(BIN2,OUTPUT);
-  pinMode(BIN1,OUTPUT);
+  pinMode(DIR1,OUTPUT);
+  pinMode(DIR2,OUTPUT);
+  pinMode(PMW1,OUTPUT);
+  pinMode(PMW2,OUTPUT);
+  
+
+  // Set these to low, I totally fried my first board because the pins were floating HIGH at startup
+  digitalWrite(DIR1,LOW);
+  digitalWrite(DIR2,LOW);
+  digitalWrite(PMW1,LOW);
+  digitalWrite(PMW2,LOW);
+
 
 
   Serial.begin(115200);
@@ -98,24 +100,22 @@ void setup(void) {
   LOG_PRINTLN("Adafruit MPU6050 test!");
 
   // Try to initialize!
-  if (!mpu.begin()) {
-    LOG_PRINTLN("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
-  LOG_PRINTLN("MPU6050 Found!");
+ // if (!mpu.begin()) {
+ //   LOG_PRINTLN("Failed to find MPU6050 chip");
+ //   while (1) {
+ //     delay(10);
+ //   }
+ // }
+ // LOG_PRINTLN("MPU6050 Found!");
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+ // mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+ // mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+ // mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
 
 
-  LOG_PRINTLN("");
+ // LOG_PRINTLN("");
   delay(100);
 
-  ledcAttachChannel(PWMA,FREQ,RESOLUTION,PWMCHANNEL);
-  ledcAttachChannel(PWMB,FREQ,RESOLUTION,PWMCHANNEL);
 
   delay(1000);
 
@@ -123,46 +123,16 @@ void setup(void) {
 
 
 void loop() {
+  
   ElegantOTA.loop();
-  LOG_PRINTf("Everything was Sucessful!");
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+  LOG_PRINTf("Everything was Sucessful!3\n");
+  //sensors_event_t a, g, temp;
+  //mpu.getEvent(&a, &g, &temp);
   
-  /* Print out the values */
- // Serial.print("Acceleration X: ");
- // Serial.print(a.acceleration.x);
- // Serial.print(", Y: ");
- // Serial.print(a.acceleration.y);
- // Serial.print(", Z: ");
- // Serial.print(a.acceleration.z);
- // LOG_PRINTLN(" m/s^2");
-
- // Serial.print("Rotation X: ");
- // Serial.print(g.gyro.x);
- // Serial.print(", Y: ");
- // Serial.print(g.gyro.y);
- // Serial.print(", Z: ");
- // Serial.print(g.gyro.z);
- // LOG_PRINTLN(" rad/s");
-
- // Serial.print("Temperature: ");
- // Serial.print(temp.temperature);
- // LOG_PRINTLN(" degC");
-
- // LOG_PRINTLN("");
-  delay(100);
-  
-
-  digitalWrite(AIN2,HIGH);
-  digitalWrite(AIN1,LOW);
-  delay(10000);
-  
-
-
-
-
-
-
+  delay(5000);
+   
+  digitalWrite(PMW1,HIGH);
+  digitalWrite(DIR1,HIGH);
 
 
 
